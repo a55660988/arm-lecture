79d6c357eba3b025bc167da80e58a6ada7ae43cd
Danny Deng <a55660988@gmail.com>
2015-03-08 17:32:17 +0800

    example1 done

--- example1/example1.c
+++ example1/example1.c
@@ -1,15 +1,22 @@
 int add(int, int);
+int div(int, int);
 
 int main()
 {
 	int a, b, c;
 	a=10;
 	b=12;
-	c=add(a,b);
-	return 0;
+	//c=add(a,b);
+	c=div(a,b);
+	//printf("%d\n", c);
+	return c;
 }
 
 int add(int a, int b)
 {
 	return a+b;
+}
+int div(int a, int b)
+{
+	return a/b;
 }
