7c9e5c7aff6ba28a07c672abe93fc5a0ff0dca66
Danny Deng <a55660988@gmail.com>
2015-03-09 19:53:03 +0800

    Implement fibonacci with non-recursive method
    Work done.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,31 +13,27 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	@ r0 is input from user
+	@ previous is -1
+	mov r3, #(-1)
+	@ result = 1
+	mov r4, #1
+	@ ensure r5 for sum is initialized as zero
+	mov r5, #0
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
+	.loop:
+		add r5, r4, r3
+		mov r3, r4
+		mov r4, r5
+		@ check loop condition
+		subs r0, #1
+		ite lt
+		movlt r0, r3
+		bge .loop
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
