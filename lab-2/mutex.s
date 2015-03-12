dd39aaa131c78e6b0190cda825cea6757dd3c1d3
Danny Deng <a55660988@gmail.com>
2015-03-13 02:51:22 +0800

    Implement simple mutex in assembly with threads in C
    Work done.

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -10,6 +10,18 @@
 lock_mutex:
         @ INSERT CODE BELOW
 
+		ldr r1, =locked @ load const locked into reg1
+		
+		@ for lock, we need to repeat check until unlock
+		@ otherwise it may destroy critical section 
+		@ for instance, we use if not while, when another 
+		@ process occupies cpu after if then go back
+		.lock_loop:
+			ldrex r2, [r0]
+			cmp r2, unlocked
+				strex r2, r1, [r0]
+				cmpeq r2, unlocked
+				bne .lock_loop @ if not unlock, continue loop check
         @ END CODE INSERT
 	bx lr
 
@@ -18,8 +30,9 @@ lock_mutex:
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
-	@ INSERT CODE BELOW
-        
+		@ INSERT CODE BELOW
+		ldr r1, =unlocked @ load const unlocked into reg1
+		str r1, [r0]
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
