e9738b9a4b8b42d476535c0b3a540bd1a91e0c80
Danny Deng <a55660988@gmail.com>
2015-03-13 04:02:53 +0800

    Follow consistent coding style
    Suggested by Jim Huang.

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -8,21 +8,22 @@
 	.global lock_mutex
 	.type lock_mutex, function
 lock_mutex:
-        @ INSERT CODE BELOW
+    @ INSERT CODE BELOW
 
-		ldr r1, =locked @ load const locked into reg1
+	ldr r1, =locked @ load const locked into reg1
 		
-		@ for lock, we need to repeat check until unlock
-		@ otherwise it may destroy critical section 
-		@ for instance, we use if not while, when another 
-		@ process occupies cpu after if then go back
-		.lock_loop:
-			ldrex r2, [r0]
-			cmp r2, unlocked
-				strex r2, r1, [r0]
-				cmpeq r2, unlocked
-				bne .lock_loop @ if not unlock, continue loop check
-        @ END CODE INSERT
+	@ for lock, we need to repeat check until unlock
+	@ otherwise it may destroy critical section 
+	@ for instance, we use if not while, when another 
+	@ process occupies cpu after if then go back
+	.lock_loop:
+		ldrex r2, [r0]
+		cmp r2, unlocked
+		strex r2, r1, [r0]
+		cmpeq r2, unlocked
+		bne .lock_loop @ if not unlock, continue loop check
+
+    @ END CODE INSERT
 	bx lr
 
 	.size lock_mutex, .-lock_mutex
@@ -30,10 +31,10 @@ lock_mutex:
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
-		@ INSERT CODE BELOW
-		ldr r1, =unlocked @ load const unlocked into reg1
-		str r1, [r0]
-        @ END CODE INSERT
+	@ INSERT CODE BELOW
+	ldr r1, =unlocked @ load const unlocked into reg1
+	str r1, [r0]
+    @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
 
